6da4e53d4e34b812051555eba5c119d3f0e44c5e
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2019-02-20 17:10:07 +0800

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="7-diet-secrets-of-stars.html"}
 if(x=="2"){xxx="multivitamins-ticket-to-daily-health.html"}
